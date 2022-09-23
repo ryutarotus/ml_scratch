fe297e265be4fccb5949c9f103cc7366753f4fab
ryutarotus <ryutaro03100310@icloud.com>
2022-09-23 14:19:32 +0900

    add linear regression

--- models/.ipynb_checkpoints/linear_regression-checkpoint.py
+++ models/.ipynb_checkpoints/linear_regression-checkpoint.py
@@ -1,2 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+class Liner_Regression():
+    def __init__(self):
+        self.intercept = None
+        self.coef = None
+        
+    def fit(self, x, y):
+        x = x.astype('float64')
+        y = y.astype('float64')
+        coef = np.cov(x, y)[0][1] / np.var(x)
+        intercept = np.mean(y) - coef * np.mean(x)
+        
+        self.coef = coef
+        self.intercept = intercept
+        return None
+    
+    def forward(self, x):
+        if self.intercept == None or self.coef == None:
+            print('Please fit model !')
+            return 'process is interruputed'
+        else:
+            x = x.astype('float64')
+            y = self.coef * x + self.intercept
+        return y
+    
+    def plot_result(self, x, y, pred):
+        plt.scatter(w, l)
+        plt.plot(w, pred)
+        plt.show()
