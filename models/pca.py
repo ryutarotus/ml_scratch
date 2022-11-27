df1996a4d1954018e6e70ab0435a95df1236f07c
ryutarotus <ryutaro03100310@icloud.com>
2022-11-27 13:12:42 +0900

    check pca

--- models/pca.py
+++ models/pca.py
@@ -6,43 +6,33 @@ class PCA():
         self.W = None
         
     def fit(self, X):
-        # 分散共分散行列の作成
-        cov_mat = np.cov(X.T)
-        # 分散共分散行列の固有値、固有ベクトルを作成
-        eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-        # 固有値、固有ベクトルのペアを作成
+        cov_mat = np.cov(X.T)  # 分散共分散行列
+        eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)  # 固有値、固有ベクトル
         eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
-        # 上記のペアを固有値 (寄与率) の大きい順にソート
-        eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+        eigen_pairs.sort(key=lambda k: k[0], reverse=True)  # 寄与率の大きい順にソート
         
         # 寄与率の高い順に固有ベクトルをn_components分獲得
         ws = []
         for i in range(self.n_components):
             ws.append(eigen_pairs[i][1])
-        # 射影行列の作成
-        self.W = np.array(ws).T
+        
+        self.W = np.array(ws).T  # 射影行列の作成
         
     def transform(self, X):
-        # 射影
-        X_pca = X @ self.W
+        X_pca = X @ self.W  # 射影
         return X_pca
     
     def fit_transform(self, X):
-        # 分散共分散行列の作成
-        cov_mat = np.cov(X.T)
-        # 分散共分散行列の固有値、固有ベクトルを作成
-        eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-        # 固有値、固有ベクトルのペアを作成
+        cov_mat = np.cov(X.T)  # 分散共分散行列
+        eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)  # 固有値、固有ベクトル
         eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
-        # 上記のペアを固有値の大きい順にソート
-        eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+        eigen_pairs.sort(key=lambda k: k[0], reverse=True)  # 寄与率の大きい順にソート
         
         # 寄与率の高い順に固有ベクトルをn_components分獲得
         ws = []
         for i in range(self.n_components):
             ws.append(eigen_pairs[i][1])
-        # 射影行列の作成
-        self.W = np.array(ws).T
-        # 射影
-        X_pca = X @ self.W
+        
+        self.W = np.array(ws).T  # 射影行列の作成
+        X_pca = X @ self.W  # 射影
         return X_pca
